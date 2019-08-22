ed0713f4d066d85d6b3e0955ce649d732a823390
Ramadani <email.ramadani@gmail.com>
2019-08-22 20:13:25 +0700

    move null date to date file

--- date.go
+++ date.go
@@ -62,3 +62,63 @@ func (d *Date) UnmarshalJSON(data []byte) error {
 	d.Time = t
 	return nil
 }
+
+func (d *NullDate) String() string {
+	if !d.Valid {
+		return ""
+	}
+
+	return d.Time.Format(dateFormat)
+}
+
+// Value null date driver value
+func (d NullDate) Value() (driver.Value, error) {
+	if !d.Valid {
+		return nil, nil
+	}
+	return d.String(), nil
+}
+
+// Scan null date scanner
+func (d *NullDate) Scan(value interface{}) error {
+	if value == nil {
+		return nil
+	}
+
+	date := value.(string)
+	t, err := time.Parse(dateFormat, date)
+	if err != nil {
+		return err
+	}
+
+	d.Time = t
+	return nil
+}
+
+// MarshalJSON null date type
+func (d NullDate) MarshalJSON() ([]byte, error) {
+	date := "null"
+	if d.Valid {
+		date = d.String()
+	}
+
+	res := fmt.Sprintf("\"%s\"", date)
+	return []byte(res), nil
+}
+
+// UnmarshalJSON date type
+func (d *NullDate) UnmarshalJSON(data []byte) error {
+	var date string
+	if err := json.Unmarshal(data, &date); err != nil {
+		return nil
+	}
+
+	t, err := time.Parse(dateFormat, date)
+	if err != nil {
+		return nil
+	}
+
+	d.Valid = true
+	d.Time = t
+	return nil
+}
